41a66b47f18bd68e75a52a1b1f83a0407a5598be
Artur Mizera <arturmiz@users.noreply.github.com>
2018-04-15 18:55:30 +0200

    test(header): snapshot tests

---
+++ __tests__/header.test.js
@@ -0,0 +1,56 @@
+import { mount } from 'vue-test-utils';
+import Header from '@/components/Header';
+
+describe('Header', () => {
+  let wrapper;
+
+  describe('by default', () => {
+
+    beforeAll(() => {
+      wrapper = mount(Header);
+    });
+
+    test('renders correctly', () => {
+      expect(wrapper.html()).toMatchSnapshot();
+    });
+
+  });
+
+  test('can have only main header content set', () => {
+    wrapper = mount(Header, {
+      slots: {
+        default: 'Header'
+      }
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test('can have main header content set using named slot', () => {
+    wrapper = mount(Header, {
+      slots: {
+        header: 'Header'
+      }
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test('can have both header and subheader content set', () => {
+    wrapper = mount(Header, {
+      slots: {
+        header: 'Header',
+        subheader: 'Subheader'
+      }
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test('can have only subheader content set', () => {
+    wrapper = mount(Header, {
+      slots: {
+        subheader: 'Subheader'
+      }
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+});
