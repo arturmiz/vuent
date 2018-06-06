b0aba47af90400b2870fa2f75acd2340c506a418
Artur Mizera <arturmiz@users.noreply.github.com>
2018-06-06 23:26:17 +0200

    docs: home page; init config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: 'Vuent',
+
+  evergreen: true,
+
+  themeConfig: {
+    repo: 'arturmiz/vuent',
+
+    sidebarDepth: 4,
+
+    sidebar: [
+      ['/', 'Introduction'],
+    ],
+
+    docsDir: 'docs',
+
+    editLinks: true,
+
+  }
+};
