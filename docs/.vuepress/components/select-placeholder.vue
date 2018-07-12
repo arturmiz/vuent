baeb16e41d389429c3d2ff997564d2720ebdc061
Artur Mizera <arturmiz@users.noreply.github.com>
2018-07-13 00:39:25 +0200

    feat(select): placeholder support (#23)

---
+++ docs/.vuepress/components/select-placeholder.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="playground">
+    <div style="max-width: 300px">
+      <vnt-select v-model="country" :options="countries" placeholder="Choose country"></vnt-select>
+    </div>
+    <div class="result">country: {{ country }}</div>
+  </div>
+</template>
+
+<script>
+import { VntSelect } from '../../../src/components';
+
+export default {
+  name: 'SelectPlaceholder',
+  components: {
+    VntSelect
+  },
+  data() {
+    return {
+      country: null,
+      countries: [
+        { label: 'Australia', value: 'AUS' },
+        { label: 'Belgium', value: 'BEL' },
+        { label: 'Canada', value: 'CAN' },
+        { label: 'Germany', value: 'GER' },
+        { label: 'France', value: 'FRA' },
+        { label: 'United Kingdom', value: 'GBR' },
+        { label: 'United States', value: 'USA' }
+      ]
+    }
+  }
+}
+</script>
