c81a9dfdf84b6092f47b6e6be52f7c067990c4f0
Artur Mizera <arturmiz@users.noreply.github.com>
2018-11-10 12:26:10 +0100

    test(tabs): covered rendering and switch on click

--- __tests__/tabs.test.js
+++ __tests__/tabs.test.js
@@ -24,4 +24,54 @@ describe('Tabs', () => {
 
   });
 
+  describe('generates correct stucture', () => {
+
+    beforeAll(() => {
+      wrapper = mount(VntTabs, {
+        slots: {
+          default: `
+            <vnt-tab label="Pivot Item 1" active>
+              <div>Tab 1</div>
+            </vnt-tab>
+            <vnt-tab label="Pivot Item 2">
+              <p>Tab 2</p>
+            </vnt-tab>
+            <vnt-tab label="Tab 3">
+              <p>Tab 3</p>
+            </vnt-tab>`
+        }
+      });
+    });
+
+    test('renders correctly', () => {
+      expect(wrapper.html()).toMatchSnapshot();
+    });
+
+  });
+
+  describe('once a tab is clicked', () => {
+
+    beforeAll(() => {
+      wrapper = mount(VntTabs, {
+        slots: {
+          default: `
+            <vnt-tab label="Pivot Item 1" active>
+              <p>Tab 1</p>
+            </vnt-tab>
+            <vnt-tab label="Pivot Item 2">
+              <p>Tab 2</p>
+            </vnt-tab>`
+        }
+      });
+
+      const label = wrapper.findAll('.vnt-tabs__header').at(1);
+      label.trigger('click');
+    });
+
+    test('renders correctly', () => {
+      expect(wrapper.html()).toMatchSnapshot();
+    });
+
+  });
+
 });
