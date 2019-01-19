538916b4990a0fc701547aa7147ac40f0cef908a
Artur Mizera <arturmiz@users.noreply.github.com>
2019-01-19 20:44:48 +0100

    docs(dialog): website docs (#49)

---
+++ docs/.vuepress/components/dialog-basic.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="playground">
+    <vnt-button :click="showDialog">Open Dialog</vnt-button>
+
+    <div class="result"><br />opened: {{ opened }}<br />result: {{ result }}</div>
+
+    <vnt-dialog
+      title="Basic dialog"
+      content="This is a basic dialog. As default it has only 'Cancel' button."
+      :opened.sync="opened"
+      v-on:result="onresult" />
+
+  </div>
+</template>
+
+<script>
+import DialogMixin from './dialog.mixin';
+
+export default {
+  name: 'DialogBasic',
+  mixins: [
+    DialogMixin
+  ]
+}
+</script>
