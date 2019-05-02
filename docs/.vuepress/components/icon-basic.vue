328f4eda45c1bb63ce8ec27f4bf9c303a0317cb1
Artur Mizera <arturmiz@users.noreply.github.com>
2019-05-02 22:44:14 +0200

    docs(icon): initial implementation docs

---
+++ docs/.vuepress/components/icon-basic.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="playground">
+    <vnt-icon name="contact" />
+  </div>
+</template>
+
+<script>
+import { VntIcon } from '../../../src/components';
+
+export default {
+  name: 'IconBasic',
+  components: {
+    VntIcon
+  }
+}
+</script>
