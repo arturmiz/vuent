5f0020e665c0158404f18fe07ee6e518322fba36
Artur Mizera <arturmiz@users.noreply.github.com>
2019-07-17 22:55:14 +0200

    feat: more storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,10 @@
-import { configure } from '@storybook/vue';
+import { configure, addDecorator } from '@storybook/vue';
+import { withA11y } from '@storybook/addon-a11y';
+
+import Vue from 'vue';
+import Vuent from '../src/Vuent'
+
+Vue.use(Vuent);
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
@@ -6,4 +12,5 @@ function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
+addDecorator(withA11y)
 configure(loadStories, module);
