699f00644986b29ea2f5fc037c540da0045cb91f
Artur Mizera <arturmiz@users.noreply.github.com>
2019-10-04 23:16:40 +0200

    docs(listview): initial implementation (#91)

---
+++ docs/.vuepress/components/listview-basic.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="playground">
+    <div style="max-width: 300px">
+      <vnt-listview>
+        <vnt-listview-item v-for="fruit in fruits" :key="fruit">
+          {{ fruit }}
+        </vnt-listview-item>
+      </vnt-listview>
+    </div>
+  </div>
+</template>
+
+<script>
+import VntListView from '../../../src/components/listview/ListView.vue';
+import VntListViewItem from '../../../src/components/listview/ListViewItem.vue';
+
+export default {
+  name: 'ListviewBasic',
+  components: {
+    VntListview: VntListView,
+    VntListviewItem: VntListViewItem
+  },
+  data() {
+    return {
+      fruits: [
+        'Banana',
+        'Apple',
+        'Raspberry',
+        'Orange',
+        'Peach',
+        'Strawberry'
+      ]
+    }
+  }
+}
+</script>
---
+++ docs/.vuepress/components/listview-selection-single.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="playground">
+    <div style="max-width: 300px">
+      <vnt-listview selectionMode="single">
+        <vnt-listview-item v-for="fruit in fruits" :key="fruit" :click="pickFruit">
+          {{ fruit }}
+        </vnt-listview-item>
+      </vnt-listview>
+    </div>
+  </div>
+</template>
+
+<script>
+import VntListView from '../../../src/components/listview/ListView.vue';
+import VntListViewItem from '../../../src/components/listview/ListViewItem.vue';
+
+export default {
+  name: 'ListviewSelectionSingle',
+  components: {
+    VntListview: VntListView,
+    VntListviewItem: VntListViewItem
+  },
+  methods: {
+    pickFruit(event, fruit) {
+      window.alert(fruit)
+    }
+  },
+  data() {
+    return {
+      fruits: [
+        'Banana',
+        'Apple',
+        'Raspberry',
+        'Orange',
+        'Peach',
+        'Strawberry'
+      ]
+    }
+  }
+}
+</script>
