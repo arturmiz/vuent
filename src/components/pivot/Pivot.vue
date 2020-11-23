dd67c4f013587d92bdb06bee2714c2e18129f0e8
Chase Ingebritson <chase.ingebritson2014@gmail.com>
2020-11-23 13:55:37 -0600

    fix(pivot): pivot-item active respects prop value

--- src/components/pivot/Pivot.vue
+++ src/components/pivot/Pivot.vue
@@ -75,7 +75,7 @@ export default {
       .filter(pivotItem => pivotItem.tag === 'vnt-pivot-item')
       .forEach((pivotItem, index) => {
         const attrs = pivotItem.data.attrs;
-        const isActive = Object.keys(attrs).indexOf('active') > -1;
+        const isActive = attrs && !!attrs.active
 
         const header = createElement('li', {
           staticClass: 'vnt-pivot__header',
